fdf186480823f2853cd13e406e86217b288377d0
codifies <nospam@antispam.com>
2015-01-15 13:45:22 +0000

    added some NULL object checking

--- jni/nvg.c
+++ jni/nvg.c
@@ -1,6 +1,7 @@
 #include <stdio.h> // for lameass debugging (printf)
 #include <assert.h> // slightly less lame debugging
 
+
 #include "stdlib.h"
 
 #include <GLES2/gl2.h>
@@ -9,31 +10,55 @@
 #include "nvg.h"
 #include "nanovg.h"
 
-
-
 #include "nanovg_gl.h"
+
+
+#define checkJO(JO, MSG) if (JO==NULL) { printf(MSG); fflush(stdout); return; }
+
+
+// used for debuging : from the gnu libc manual 
+/*
+#include <execinfo.h>
+void print_trace (void)
+{
+  void *array[10];
+  size_t size;
+  char **strings;
+  size_t i;
+
+  size = backtrace (array, 10);
+  
+  strings = backtrace_symbols (array, size);
+
+  printf ("Obtained %zd stack frames.\n", size);
+
+  for (i = 0; i < size; i++)
+     printf ("%s\n", strings[i]);
+
+  fflush(stdout);
+  free (strings);
+}
+*/
 
 // both nvg.Paint and nvg.RGBA have a field "buff" this gets the 
 // physical address of this buffer
 void* getBuffPtr(JNIEnv *e, jobject jo)
 {
-	jobject paintjo = (*e)->GetObjectClass(e, jo);
-	assert(paintjo != NULL);
-	jfieldID fid = (*e)->GetFieldID(e, paintjo,"buff","Ljava/nio/FloatBuffer;");
-	assert(fid != NULL);
+	jobject cls = (*e)->GetObjectClass(e, jo);
+	jfieldID fid = (*e)->GetFieldID(e, cls,"buff","Ljava/nio/FloatBuffer;"); // this is an internal field so should get changed!
 	jobject jbuff = (*e)->GetObjectField(e, jo, fid);
-	assert(jbuff != NULL);
 	void* ptr = (void*)(*e)->GetDirectBufferAddress(e, jbuff);
-	assert(ptr != NULL);
-	// phew!
 	return ptr;	
 }
 
 // given a jni env and NVGpaint this creates a java object
 // encapsulating the NVGpaint structure (nvg.Paint)
+
+/* depricated
 jobject createPaintJobject(JNIEnv *e, NVGpaint p) {
-	jclass cls = (*e)->FindClass(e, "nvg$Paint");
+	jclass cls = (*e)->FindClass(e, "nvg$Paintr");
 	assert(cls != NULL);
+
 	jmethodID constructor = (*e)->GetMethodID(e, cls, "<init>", "()V");
 	assert(constructor != NULL);
 	
@@ -49,7 +74,7 @@ jobject createPaintJobject(JNIEnv *e, NVGpaint p) {
 	*ptr = p;
 	return nvgpaint;
 }
-
+*/
 JNIEXPORT jlong JNICALL Java_nvg_test
   (JNIEnv *e, jclass c, jint i)
 {
@@ -114,10 +139,10 @@ JNIEXPORT void JNICALL Java_nvg_lineTo
 	nvgLineTo((NVGcontext*)ctx,x,y);
 }
 
-
 JNIEXPORT void JNICALL Java_nvg_fillColor
   (JNIEnv *e, jclass c, jlong ctx, jobject jo)
 {
+	checkJO(jo,"\nNull colour was passed to fillColor\n");
 	nvgFillColor((NVGcontext*)ctx, (NVGcolor)*((NVGcolor*)getBuffPtr(e,jo)));
 }
 
@@ -130,6 +155,7 @@ JNIEXPORT void JNICALL Java_nvg_fillColorf
 JNIEXPORT void JNICALL Java_nvg_strokeColor
   (JNIEnv *e, jclass c, jlong ctx, jobject jo)
 {
+	checkJO(jo,"\nNull colour was passed to strokeColor\n");
 	nvgStrokeColor((NVGcontext*)ctx, (NVGcolor)*((NVGcolor*)getBuffPtr(e,jo)));
 }
 
@@ -236,19 +262,12 @@ JNIEXPORT void JNICALL Java_nvg_roundedRect
 	nvgRoundedRect((NVGcontext*)ctx, x,y, w,h, r);
 }
 
-/*
-JNIEXPORT jobject JNICALL Java_nvg_imagePattern
-  (JNIEnv *e, jclass c, jlong ctx, jfloat ox, jfloat oy, jfloat ex, jfloat ey, jfloat angle, jint image, jfloat alpha)
-{
-	NVGpaint p = nvgImagePattern((NVGcontext*)ctx, ox, oy, ex, ey, angle, image, alpha);
-	return createPaintJobject(e, p);
-}
-*/
 
 JNIEXPORT void JNICALL Java_nvg__1_1imagePattern
   (JNIEnv *e, jclass c, jlong ctx, jobject paint, jfloat ox, jfloat oy,
 		jfloat ex, jfloat ey, jfloat angle, jint image, jfloat alpha)
 {
+	// paint can't be null as it's passed internally as the objects instance as a convenience 
 	NVGpaint p = nvgImagePattern((NVGcontext*)ctx, ox, oy, ex, ey, angle, image, alpha);
 	NVGpaint* dp = (NVGpaint*)getBuffPtr(e,paint);
 	*dp = p;
@@ -257,6 +276,8 @@ JNIEXPORT void JNICALL Java_nvg__1_1imagePattern
 JNIEXPORT void JNICALL Java_nvg__1_1linearGradient
   (JNIEnv *e, jclass cls, jlong ctx, jobject paint, jfloat sx, jfloat sy, jfloat ex, jfloat ey, jobject scol, jobject ecol)
 {
+	checkJO(scol,"\nLinearGradient was passed NULL start colour\n");
+	checkJO(ecol,"\nLinearGradient was passed NULL end colour\n");
 	NVGpaint p = nvgLinearGradient((NVGcontext*)ctx,sx,sy,ex,ey,
 					(NVGcolor)*((NVGcolor*)getBuffPtr(e,scol)),
 					(NVGcolor)*((NVGcolor*)getBuffPtr(e,ecol)));
@@ -268,6 +289,8 @@ JNIEXPORT void JNICALL Java_nvg__1_1boxGradient
   (JNIEnv *e, jclass cls, jlong ctx, jobject paint, jfloat x, jfloat y, 
 				jfloat w, jfloat h, jfloat r, jfloat f, jobject scol, jobject ecol)
 {
+	checkJO(scol,"\nBoxGradient was passed NULL start colour\n");
+	checkJO(ecol,"\nBoxGradient was passed NULL end colour\n");
 	NVGpaint p = nvgBoxGradient((NVGcontext*)ctx, x, y, w, h, r, f, 
 					(NVGcolor)*((NVGcolor*)getBuffPtr(e,scol)),
 					(NVGcolor)*((NVGcolor*)getBuffPtr(e,ecol)));
@@ -278,6 +301,8 @@ JNIEXPORT void JNICALL Java_nvg__1_1boxGradient
 JNIEXPORT void JNICALL Java_nvg__1_1radialGradient
   (JNIEnv *e, jclass c, jlong ctx, jobject paint, jfloat cx, jfloat cy, jfloat inr, jfloat outr, jobject scol, jobject ecol)
 {
+	checkJO(scol,"\nRadialGradient was passed NULL start colour\n");
+	checkJO(ecol,"\nRadialGradient was passed NULL end colour\n");
 	NVGpaint p = nvgRadialGradient((NVGcontext*)ctx, cx, cy, inr, outr,
 					(NVGcolor)*((NVGcolor*)getBuffPtr(e,scol)),
 					(NVGcolor)*((NVGcolor*)getBuffPtr(e,ecol)));
@@ -287,7 +312,8 @@ JNIEXPORT void JNICALL Java_nvg__1_1radialGradient
   
 JNIEXPORT void JNICALL Java_nvg_fillPaint
   (JNIEnv *e, jclass c, jlong ctx, jobject paint)
-{  
+{
+	checkJO(paint,"\nFillPaint was passed NULL paint\n");  
 	nvgFillPaint((NVGcontext*)ctx, (NVGpaint)*((NVGpaint*)getBuffPtr(e,paint)));
 }
 
@@ -318,6 +344,7 @@ JNIEXPORT void JNICALL Java_nvg_fontSize
 JNIEXPORT void JNICALL Java_nvg_fontFace
   (JNIEnv *e, jclass c, jlong ctx, jstring jfont)
 {
+	checkJO(jfont, "\nfontFace was passed a NULL string\n");
     const char *font = (*e)->GetStringUTFChars(e, jfont, 0);
 
 	nvgFontFace((NVGcontext*)ctx, font);	
@@ -340,6 +367,7 @@ JNIEXPORT void JNICALL Java_nvg_fontBlur
 JNIEXPORT jfloat JNICALL Java_nvg_text
   (JNIEnv *e, jclass c, jlong ctx, jfloat x, jfloat y, jstring jmesg)
 {
+	checkJO(jmesg,"\nvg.Text was passed a NULL string\n");
     const char *mesg = (*e)->GetStringUTFChars(e, jmesg, 0);
     	
 	float r=nvgText((NVGcontext*)ctx, x,y, mesg, NULL);
@@ -351,9 +379,12 @@ JNIEXPORT jfloat JNICALL Java_nvg_text
 JNIEXPORT jint JNICALL Java_nvg_createFont
   (JNIEnv *e, jclass c, jlong ctx, jstring jname, jstring jpath)
 {
+	checkJO(jname,"\ncreateFont was passed a NULL name string\n");
+	checkJO(jpath,"\ncreateFont was passed a NULL path string\n");
+	
 	const char *name = (*e)->GetStringUTFChars(e, jname, 0);
 	const char *path = (*e)->GetStringUTFChars(e, jpath, 0);
-
+	
 	int r = nvgCreateFont((NVGcontext*)ctx, name, path);
 
     (*e)->ReleaseStringUTFChars(e, jname, name);
@@ -389,6 +420,7 @@ JNIEXPORT void JNICALL Java_nvg_scale
 JNIEXPORT void JNICALL Java_nvg_strokePaint
   (JNIEnv *e, jclass c, jlong ctx, jobject paint)
 {
+	checkJO(paint,"\nstrokePaint was passed a NULL paint\n");
 	nvgStrokePaint((NVGcontext*)ctx, (NVGpaint)*((NVGpaint*)getBuffPtr(e,paint)));
 }
 
@@ -455,6 +487,7 @@ JNIEXPORT void JNICALL Java_nvg_textLineHeight
 JNIEXPORT jint JNICALL Java_nvg_findFont
   (JNIEnv *e, jclass c, jlong ctx, jstring jname)
 {
+	checkJO(jname,"\nfindFont was passed a NULL name string\n");
 	const char *name = (*e)->GetStringUTFChars(e, jname, 0);
 
 	int r = nvgFindFont((NVGcontext*)ctx, name);
